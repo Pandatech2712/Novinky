9922ddf8239684446e02cab608fa5a607eeaaded
Pandatech2712 <144889346+Pandatech2712@users.noreply.github.com>
2024-02-21 18:55:03 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const bodyElement = document.querySelector('body');
+body.style.backgroundColor = #e9e9e9
+
+const newsElement = document.querySelector('.news');
+news.style.backgroundColor = 'white';
+news.style.maxWidth = '60rem'
+
+const h1Element = document.querySelector('h1');
+h1.classList.add('news__title');
+h1.textContent = 'Aktuální novinky';
+
+const firstNewsElement = document.getElementById("#zprava1"); 
+
+zprava1.classList.add('post--main');
+
+
+const thirdNewsImage = document.querySelector('#zprava3 img');
+thirdNewsImage.src = 'images/zprava3-novy.jpg';
